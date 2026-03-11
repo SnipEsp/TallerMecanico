485479f887da8220f419ab59a8bb0121a1939901
SnipEsp <ferrari300cc@gmail.com>
2026-03-11 09:19:32 +0100

    He terminado la clase Revision.java

--- src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/ClienteTest.java
+++ src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/ClienteTest.java
@@ -1,6 +1,5 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
-import org.iesalandalus.programacion.tallermecanico.modelo.Cliente;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
--- src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/RevisionTest.java
+++ src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/RevisionTest.java
@@ -1,6 +1,5 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
-import org.iesalandalus.programacion.tallermecanico.modelo.Cliente;
 import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +8,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
