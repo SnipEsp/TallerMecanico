26a0129385ee4264c745f3c2f66fca830aea84cf
SnipEsp <ferrari300cc@gmail.com>
2026-03-11 19:38:56 +0100

    He creado la clase Clientes.java

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+
+import java.util.ArrayList;
+
+public class Clientes {
+    ArrayList<Cliente> listaClientes = new ArrayList<>();
+
+    public void get() {
+        Cliente[ *];
+    }
+}
